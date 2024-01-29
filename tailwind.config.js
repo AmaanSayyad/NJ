13176e1537181080511241ebfbde3a61284ce5be
Amaan Sayyad <amaansayyad@yahoo.com>
2024-01-29 23:01:18 +0530

    add assets

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const { colors } = require("tailwindcss/colors")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-bg": "url('/svgs/nj-gradient-bg.svg')",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      colors: {
+        ...colors,
+        white: "#F5F5F5",
+        accent: "#63A2DC",
+        "neutral-light": "#3C4142",
+        "neutral-mid": "#2A2B2C",
+        "neutral-dark": "#202323",
+        "neutral-darkest": "#1A1D1D",
+        brand: {
+          50: "#f3f3f3",
+          100: "#e7e7e7",
+          200: "#c4c4c4",
+          300: "#a0a0a0",
+          400: "#585858",
+          500: "#111111",
+          600: "#0f0f0f",
+          700: "#0d0d0d",
+          800: "#0a0a0a",
+          900: "#080808",
+          DEFAULT: "#111111",
+        },
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+}
