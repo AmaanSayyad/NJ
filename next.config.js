bf44ef7572ab9f8538f4c6250ce72ae05c888e64
Amaan Sayyad <amaansayyad@yahoo.com>
2024-01-30 01:19:12 +0530

    add tect arch png

--- next.config.js
+++ next.config.js
@@ -10,3 +10,18 @@ const nextConfig = {
 };
 
 module.exports = nextConfig
+
+
+/*⚠ Invalid next.config.js options detected: 
+⚠     Unrecognized key(s) in object: 'mdx'
+⚠ See more info here: https://nextjs.org/docs/messages/invalid-next-config
+  ▲ Next.js 14.0.1
+  - Environments: .env.local
+
+uncaughtException [Error: EPERM: operation not permitted, open 'C:\Users\amaan\OneDrive\Documents\Github\NJ\.next\trace'] {
+ errno: -4048,
+ code: 'EPERM',
+ syscall: 'open',
+ path: 'C:\\Users\\amaan\\OneDrive\\Documents\\Github\\NJ\\.next\\trace'
+}
+error Command failed with exit code 1.*/
